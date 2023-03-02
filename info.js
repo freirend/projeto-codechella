2f13f9f0d41d307007a51eae366225d0f9ab88dd
Lucas Freire <lucfre7@gmail.com>
2023-03-02 16:32:36 -0300

    finalizando section infos

---
+++ info.js
@@ -0,0 +1,38 @@
+const pergunta = document.querySelectorAll('.pergunta')
+const resposta = document.querySelectorAll('.resposta')
+const arrow = document.querySelector('.uil-angle-down')
+
+function mostrar(id){
+    if (resposta[id].className == "resposta hide"){
+        resposta[id].classList.remove('hide')
+        arrow.style.transform = 'rotate(180deg)'
+    } else{
+        resposta[id].classList.add('hide')
+        arrow.style.transform = 'rotate(0deg)'
+    }
+}
+
+for (c = 0; c < pergunta.length; c++){
+    pergunta[c].addEventListener('click', (param)=>{
+        mostrar(param.target.parentElement.id)
+    })
+}
+
+
+
+
+
+
+/*
+pergunta.addEventListener('click', mostrar)
+
+
+function mostrar(){
+    if (resposta.className == "resposta hide"){
+        resposta.classList.remove('hide')
+        arrow.style.transform = 'rotate(180deg)'
+    } else{
+        resposta.classList.add('hide')
+        arrow.style.transform = 'rotate(0deg)'
+    }
+*/
